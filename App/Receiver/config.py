5fbd8ee3c2fc99bbc6b77124c7af1ff5547787cf
MysteriousBits <21dcba12@gmail.com>
2022-08-12 17:48:34 +0600

    port name in different platforms

--- App/Receiver/config.py
+++ App/Receiver/config.py
@@ -1,4 +1,5 @@
 # Arduino port name
+# should be COM3, COM4 etc on windows and dev/tty... in linux
 port = "/dev/ttyACM0"
 # File indicator bytes set in arduino
 file_ind_bytes = "\x05"
--- App/Transmitter/config.py
+++ App/Transmitter/config.py
@@ -1,4 +1,5 @@
 # Arduino port name
+# should be COM3, COM4 etc on windows and dev/tty... in linux
 port = "/dev/ttyACM0"
 # File start indicator bytes
 file_ind_bytes = "\x05"
